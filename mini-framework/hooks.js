24a5baf006ad5f2a7652a22111586aa22fe9e116
oerraoui <itsscarl2022@gmail.com>
2025-12-19 16:31:56 +0100

    Reset hook indices for state and effect management

--- mini-framework/hooks.js
+++ mini-framework/hooks.js
@@ -1,24 +1,43 @@
 import { rerender } from "./render.js"
 
 const states = []
+const effect_dependencies = []
 let state_index = 0
+let effect_index = 0
 
 export function use_state(initial_value) {
-    const index = state_index
-
-    if (states[index] === undefined) {
-        states[index] = initial_value
+    const i = state_index
+    if (states[i] === undefined) {
+        states[i] = initial_value
     }
 
     const set_state = (value) => {
-        states[index] = value
+        states[i] = value
         rerender()
     }
 
     state_index++
-    return [states[index], set_state]
+    return [states[i], set_state]
 }
 
-export function use_effect(callback) {
-    callback()
+export function use_effect(callback, deps) {
+    const i = effect_index
+    const prev = effect_dependencies[i]
+
+    let changed = true
+    if (prev && deps) {
+        changed = deps.some((d, idx) => d !== prev[idx])
+    }
+
+    if (changed) {
+        callback()
+    }
+
+    effect_dependencies[i] = deps
+    effect_index++
+}
+
+export function reset_hook_index() {
+    state_index = 0
+    effect_index = 0
 }
